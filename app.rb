01435919e62ef4efe445ee27b7612c555148821a
Sara Spink <spinkbot@gmail.com>
2017-09-29 10:29:37 -0700

    allows the user to add brands to a store and renders the list of brands for that store

--- app.rb
+++ app.rb
@@ -24,6 +24,6 @@ post ("/store/:id") do
   @store = Store.find(params[:id].to_i)
   brand = Brand.create({:name => params["name"]})
   @store.brands.push(brand)
-binding.pry
+
   redirect("/store/#{@store.id}")
 end
