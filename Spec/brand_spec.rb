a4016db6fe10e3ec0307378cddb032e2349c4be4
Sara Spink <spinkbot@gmail.com>
2017-09-29 09:04:00 -0700

    successfully associated brands and stores

---
+++ Spec/brand_spec.rb
@@ -0,0 +1,6 @@
+require("spec_helper")
+
+
+describe(Brand) do
+  it { should have_and_belong_to_many(:stores)}
+end
---
+++ Spec/store_spec.rb
@@ -0,0 +1,6 @@
+require("spec_helper")
+
+
+describe(Store) do
+  it { should have_and_belong_to_many(:brands)}
+end
