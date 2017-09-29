4992000b854725418c85c2c5a7a9f4e28570e665
Sara Spink <spinkbot@gmail.com>
2017-09-29 09:27:20 -0700

    a user can add a store to the database

--- app.rb
+++ app.rb
@@ -3,3 +3,14 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get ("/") do
+  @stores = Store.all()
+  erb(:index)
+end
+
+post ("/") do
+  name = params["name"]
+  store = Store.create({:name => name})
+  redirect("/")
+end
